f5b1eff45411100c490db708244eb16d4f15f167
aloshhen <166549924+aloshhen@users.noreply.github.com>
2026-01-12 22:47:03 +0100

    Add src/App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect } from 'react'
+import Navigation from './components/Navigation'
+import Hero from './components/Hero'
+import Features from './components/Features'
+import About from './components/About'
+import CTA from './components/CTA'
+import Footer from './components/Footer'
+
+function App() {
+  useEffect(() => {
+    if (window.Telegram && window.Telegram.WebApp) {
+      window.Telegram.WebApp.ready()
+    }
+  }, [])
+
+  return (
+    <div className="min-h-screen bg-slate-950 text-white">
+      <Navigation />
+      <Hero />
+      <Features />
+      <About />
+      <CTA />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
